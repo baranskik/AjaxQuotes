944e79f76392a71177f87f083639b13971d436ec
Krzysztof Barański <Krzysztof Baraski>
2017-04-24 19:55:30 +0200

    pierwsza wersja-nie działa

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
+});
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
+}
+function createTweet(input) {
+	if (!input.quoteAuthor.length) {
+		input.quoteAuthor = "Unknown author";
+  }
+  var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+  if (tweetText.length > 140) {
+	getQuote();
+} else {
+	var tweet = tweetLink + encodeURIComponent(tweetText);
+  console.log(tweet);
+	$('.quote').text(input.quoteText);
+	$('.author').text("Author: " + input.quoteAuthor);
+	$('.tweet').attr('href', tweet);
+console.log(tweetText);
+}
+}
